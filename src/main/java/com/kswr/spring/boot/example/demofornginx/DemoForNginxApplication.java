fddb2b63ed59a5b6375eae54df20edc6c685d55c
kswr <ksawery.oleszynski@gmail.com>
2018-11-29 17:51:58 +0100

    Initial commit

---
+++ src/main/java/com/kswr/spring/boot/example/demofornginx/DemoForNginxApplication.java
@@ -0,0 +1,16 @@
+package com.kswr.spring.boot.example.demofornginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoForNginxApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForNginxApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/kswr/spring/boot/example/demofornginx/DemoForNginxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kswr.spring.boot.example.demofornginx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoForNginxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
